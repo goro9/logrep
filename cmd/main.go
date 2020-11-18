2182ec4d4519372c868db673c85b8b08fede9bad
goro9 <sokenbiccha@icloud.com>
2020-11-19 00:33:23 +0900

    plain grep 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	flag.Parse()
+	var dir string
+	if args := flag.Args(); len(args) == 0 {
+		fmt.Println("input target directory path")
+	} else {
+		dir = args[0]
+		logrep(dirwalk(dir))
+	}
+
+}
+
+func dirwalk(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, dirwalk(filepath.Join(dir, file.Name()))...)
+			continue
+		}
+		paths = append(paths, filepath.Join(dir, file.Name()))
+	}
+
+	sort.Strings(paths)
+	return paths
+}
+
+func logrep(paths []string) error {
+	for _, p := range paths {
+		f, err := os.Open(p)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		reader := bufio.NewReader(f)
+
+		for {
+			lb, _, err := reader.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
+
+			l := string(lb)
+			if strings.Contains(l, "W") {
+				fmt.Println(p, reader.Size()-reader.Buffered(), l)
+			}
+		}
+	}
+	return nil
 }
